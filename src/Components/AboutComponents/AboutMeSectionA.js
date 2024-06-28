4857db64917d2ce932e2812bbc04b1aa00f34730
Manuel Francisco <ciesafrancisco@hotmail.com>
2024-06-28 02:06:18 +0200

    update

--- src/Components/AboutComponents/AboutMeSectionA.js
+++ src/Components/AboutComponents/AboutMeSectionA.js
@@ -8,31 +8,28 @@ import './_AboutMeSectionA.scss';
 const AboutMeSectionA = () => (
   <section id="about-a" className="text-center py-3 my-4">
     <div className="container">
-      <h2 className="section-title" lang="fr">About Me</h2>
+      <h2 className="section-title" lang="fr">À Propos de Moi</h2>
       <p className="lead" lang="fr">
         VOTRE VISION, MON ENGAGEMENT...
       </p>
       
       <p className="lead">
-      Laissez-moi vous parler un peu de moi et de ce que je fais...
+        Laissez-moi vous parler un peu de moi et de ce que je fais...
       </p>
       <div className="about-info">
         <img src={myself} alt="Manuel Francisco" className="my-image my-image-cartoon" />
         <div className="bio bg-light">
           <h4 lang="fr">Votre projet est entre de bonnes mains</h4>
           <p lang="fr">
-            Bienvenue dans l'univers où la créativité rencontre la
-            fonctionnalité, où vos idées prennent vie sur le web. En tant
-            qu'intégrateur web passionné, je m'engage à donner vie
-            à votre projet avec une expertise méticuleuse et une passion
-            inébranlable.
+            Bienvenue dans l'univers où la créativité rencontre la fonctionnalité. Je suis [Votre Nom], un développeur web passionné avec une expertise en HTML, CSS, JavaScript, React, et Node.js. Je crée des sites web dynamiques et conviviaux depuis [X] ans.
           </p>
           <h4 lang="fr">TRANSFORMEZ VOS IDÉES EN RÉALITÉ</h4>
           <p lang="fr">
-            Laissez-moi transformer vos concepts en une présence web
-            percutante. Chaque détail compte, et je m'assure que votre
-            site internet non seulement répond à vos attentes, mais les
-            dépasse.
+            Je m'engage à transformer vos concepts en une présence web percutante. Chaque détail compte, et je m'assure que votre site internet dépasse vos attentes.
+          </p>
+          <h4 lang="fr">Toujours prêt à apprendre et à innover</h4>
+          <p lang="fr">
+            Je suis constamment à la recherche de nouvelles opportunités pour grandir et collaborer avec des professionnels partageant les mêmes idées. N'hésitez pas à me contacter pour discuter de votre prochain projet web !
           </p>
         </div>
         <div className="row-1">
@@ -56,8 +53,7 @@ const AboutMeSectionA = () => (
           />
           <h3 lang="fr">Vous pouvez suivre tout le processus</h3>
           <p lang="fr">
-            Exploration des esquisses UI/UX  la base d'interfaces
-            utilisateur intuitives et engageantes
+            Exploration des esquisses UI/UX la base d'interfaces utilisateur intuitives et engageantes
           </p>
         </div>
         <div className="row-3">
@@ -69,8 +65,7 @@ const AboutMeSectionA = () => (
           />
           <h3 lang="fr">Utilisation des outils</h3>
           <p lang="fr">
-            Utilisation d'outils comme Photoshop, Figma ou Marvel pour
-            atteindre vos objectifs
+            Utilisation d'outils comme Photoshop, Figma ou Marvel pour atteindre vos objectifs
           </p>
         </div>
       </div>
